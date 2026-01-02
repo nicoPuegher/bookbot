e709a7275ed1b57c81073cea51876ff8495777e5
Nicolas <nicopuegher@gmail.com>
2026-01-02 19:31:25 -0300

    import count_characters in main and store its result

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from stats import count_characters
+
+
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     book_words = book_text.split()
+    character_counts = count_characters(book_words)
 
 
 def get_book_text(filepath):
