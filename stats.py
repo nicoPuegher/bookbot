4ba8f4373d462e8f6f7691a12a0bc67b577b2cad
Nicolas <nicopuegher@gmail.com>
2026-01-02 19:31:07 -0300

    define count_characters fn and return a str:int dictionary

--- stats.py
+++ stats.py
@@ -1,1 +1,13 @@
+def count_characters(book_words):
+    character_counts = {}
 
+    for word in book_words:
+        for char in word:
+            lowercase_char = char.lower()
+
+            if lowercase_char not in character_counts:
+                character_counts[lowercase_char] = 1
+            else:
+                character_counts[lowercase_char] += 1
+
+    return character_counts
